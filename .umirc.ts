e89a9e6ee740d63c7da626670afab0c5be25b081
renjie.yin <renjie.yin@ronds.com.cn>
2021-03-23 14:18:51 +0800

    feat✨ 更新文档发布配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  // base:
+  //   process.env.NODE_ENV === 'production' ? '/react-base-table-extends/' : '/',
   publicPath:
     process.env.NODE_ENV === 'production' ? '/react-base-table-extends/' : '/',
   title: 'react-base-table-extends',
+  history: { type: 'hash' },
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo:
